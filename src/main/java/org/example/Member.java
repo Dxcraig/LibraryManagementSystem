8018e067aec3646c02bdeb52d57da8a999c5e89e
Dxcraig <steviedoe20@gmail.com>
2025-01-17 13:09:03 +0000

    Added getters and setters

--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -12,5 +12,27 @@ public class Member {
         this.name = name;
     }
 
+    public String getMemberId() {
+        return memberId;
+    }
 
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(ArrayList<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
 }
