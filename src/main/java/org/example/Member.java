a6f4e3879d742c66cd1abf19ea2147f35ff00d7c
Dxcraig <steviedoe20@gmail.com>
2025-01-17 13:08:01 +0000

    Added constructor to initialize private fields

--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Member {
     private String memberId;
     private String name;
+    private ArrayList<Book> borrowedBooks;
+
+    public Member(String memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
 
 
 }
