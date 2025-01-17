bd704cf1292893b351930f2cbff6974dbc7cbebe
Dxcraig <steviedoe20@gmail.com>
2025-01-17 13:16:54 +0000

    created addbooks and register members methods

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -5,4 +5,19 @@ import java.util.ArrayList;
 public class Library {
     private ArrayList<Book> books;
     private ArrayList<Member> members;
+
+    public Library() {
+        books = new ArrayList<>();
+        members = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void addMember(Member member) {
+        members.add(member);
+    }
+
+
 }
