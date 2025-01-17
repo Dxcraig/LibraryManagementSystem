fbe888ec75eec03f09384606cdc93d03f2fea8a0
Dxcraig <steviedoe20@gmail.com>
2025-01-17 13:02:56 +0000

    Introduced getters and setters for private fields

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -12,7 +12,37 @@ public class Book {
         this.availableCopies = availableCopies;
     }
 
+    public String getBookId() {
+        return bookId;
+    }
 
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
 
     public void borrowBook() {
         if (availableCopies > 0) {
